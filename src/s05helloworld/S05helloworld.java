a67bf3d3b2e519e16e1794c2b9f5c026c16bf8dc
alumno <alumno@LIMSFLAB04-15D.system-uap.adm>
2017-04-03 19:24:28 -0500

    first commit

---
+++ src/s05helloworld/S05helloworld.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package s05helloworld;
+
+/**
+ *
+ * @author alumno
+ */
+public class S05helloworld {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+      System.out.println("Hello World");
+      System.out.println("Antonio Orosco");
+    }
+}
